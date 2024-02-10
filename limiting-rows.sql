49307402f60453507f97cac0b9c9696a9b168145
CihanKurnaz <78209988+CihanKurnaz@users.noreply.github.com>
2024-02-10 15:51:41 +0300

    Update limiting-rows.sql

--- limiting-rows.sql
+++ limiting-rows.sql
@@ -2,3 +2,10 @@ select * from employees
 where rownum < 3;
 
 -- Returns 2 rows
+
+select empno, salary 
+  from employees
+order  by salary desc
+fetch  first 3 rows with ties;
+
+-- Returns 3 rows which are the top salaries 
